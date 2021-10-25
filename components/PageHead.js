334f74e8c636c26c22d64ebb040836a0057ab7d3
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2021-10-25 11:13:54 -0300

    chore: update og image

--- components/PageHead.js
+++ components/PageHead.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 const title = 'Retrocade Earnings Checker'
 const description = "Check how much BUSD you've earned while holding RC tokens"
-const ogImage = 'https://user-images.githubusercontent.com/16388408/138349218-4c25a8dd-55f2-4d5e-98dc-1804c3a96de3.png'
+const ogImage = 'https://user-images.githubusercontent.com/16388408/138710694-e7f74d18-4731-4a15-9833-4859491d5e35.png'
 
 export default function PageHead() {
   return (

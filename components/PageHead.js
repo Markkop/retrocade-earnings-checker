5dc9c055ce6f72e2451b04b13c0c0325289eca55
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-02-14 10:25:28 -0300

    feat: update meta description

--- components/PageHead.js
+++ components/PageHead.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 const title = 'Retrocade Rewards Tracker'
-const description = "Check how much BUSD you've earned from playing and holding RC tokens"
+const description = "Check how much BUSD you've earned from playing and staking RC tokens"
 const ogImage = 'https://user-images.githubusercontent.com/16388408/138710694-e7f74d18-4731-4a15-9833-4859491d5e35.png'
 
 export default function PageHead() {
